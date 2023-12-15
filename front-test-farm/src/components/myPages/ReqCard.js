a7f82d7030faa981cf47fe8f53fc2ea746a3e75e
JeHa-An <anjeha048@gmail.com>
2023-12-15 18:08:52 +0900

    프론트 1차 통합

--- front-test-farm/src/components/myPages/ReqCard.js
+++ front-test-farm/src/components/myPages/ReqCard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Card from '../UI/Card';
 import style from './QuotCard.module.css';
 import * as API from '../../api/index';
-import QuotCard from '../../pages/MyPages/QuotList';
+import QuotCard from '../../components/myPages/QuotCard';
 
 const ReqCard = ({ req }) => {
   //견적서 카드
@@ -38,11 +38,11 @@ const ReqCard = ({ req }) => {
 
       {quotList.length > 0
         ? quotList.map((quoteItem) => (
-            <QuotCard
-              key={quoteItem.quote.quotaionId}
-              quoteItem={quoteItem}
-            />
-          ))
+          <QuotCard
+            key={quoteItem.quote.quotaionId}
+            quoteItem={quoteItem}
+          />
+        ))
         : ''}
     </Card>
   );
--- front-test-farm/src/pages/FarmersPages/Invoice.js
+++ front-test-farm/src/pages/FarmersPages/Invoice.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import calendar from './calendar';
 import './style/Invoice.css';
 
 const Invoice = () => {

95d6abd91bc850ddfe0190f168728683ba95d1b9
JeHa-An <anjeha048@gmail.com>
2023-12-24 01:38:25 +0900

    farmerpage/requestlist css 작업

--- front-test-farm/src/pages/FarmersPages/RequestList.js
+++ front-test-farm/src/pages/FarmersPages/RequestList.js
@@ -12,7 +12,7 @@ const RequestList = () => {
   const [interestList, setInterestList] = useState([]);
   const [reqList, setReqList] = useState([]);
   const [selInt, setSelInt] = useState();
-  
+
   const effectFunc = async () => {
     try {
       const response = await API.get(`/farmer/farmInterest`, token);
@@ -20,14 +20,14 @@ const RequestList = () => {
       console.log(data);
       setReqList([...data.reqList]);
       setInterestList([...data.interestList]);
-      setSelInt(...data.interestList[0]);
-    } catch(error) {
+      setSelInt(data.interestList[0]);
+    } catch (error) {
       console.error('Error fetching data:', error);
     }
   }
   useEffect(() => {
     effectFunc();
-    }, []);
+  }, []);
 
   const changeInterest = async (interestOne) => {
     try {
@@ -35,7 +35,7 @@ const RequestList = () => {
       const data = response.data;
       setReqList([...data]);
       setSelInt(interestOne);
-    } catch(error) {
+    } catch (error) {
       console.error('Error fetching data:', error);
     }
   }
@@ -54,34 +54,41 @@ const RequestList = () => {
           </span>
         </div>
         <div className="dropdown">
-          <button className="dropbtn">{selInt}</button>
+          <button className="dropbtn">관심 품목 : <span>{selInt}</span></button>
           <div className="dropdown-content">
             {
-              interestList !== null && interestList.map((interest, idx) =>
-                <a href="#" key={idx} onClick={() => changeInterest(interest)}>{interest == null ? "없음" : interest}</a>
+              interestList !== null && interestList.map((interest, idx) => (
+                interest !== null && <a href="#" key={idx} onClick={() => changeInterest(interest)}>{interest}</a>
+              )
               )
             }
           </div >
         </div >
       </div >
-      {reqList.length !== 0 ? reqList.map((req) =>
-        <div className='request-box' key={req.requestId}>
-          <div className='request-content'>
-            <p>{req.name} <span>님</span></p>
-            <p>요청한 품목 : {req.requestProduct}</p>
-            <p>요청한 수량 혹은 kg : {req.requestQuantity}kg</p>
-            <p>배송지 : {req.address}</p>
+      <div className='list-container'>
+        {reqList.length > 0 ? reqList.map((req) =>
+          <div className='request-box' key={req.requestId}>
+            <div className='request-content'>
+              <p className='user-name'>{req.name} <span>님의 요청서</span></p>
+              <p className='value-name'>요청서 번호 : <span>{req.requestId}</span></p>
+              <p className='value-name'>요청한 품목 :  <span>{req.requestProduct}</span></p>
+              <p className='value-name'>요청한 kg :  <span>{req.requestQuantity}kg</span></p>
+              <p className='value-name'>배송지 :  <span>{req.address2}</span></p>
+            </div>
+            <div className='request-btn'>
+              <Link className='link-to' to={`/farmerpage/quotform/${req.requestId}/${req.requestProduct}/${req.requestQuantity}`}>견적 보내기</Link>
+              <p className='request-btn-comment'>
+                <span>{req.name} </span>
+                님에게<br /> 맛있는 농산물을 보내주세요!
+              </p>
+            </div>
           </div>
-          <div className='request-btn'>
-            <button><Link className='a' to={`/farmerpage/quotform/${req.requestId}/${req.requestProduct}/${req.requestQuantity}`}>견적 보내기</Link></button>
-            <p>요청서 번호 : {req.requestId}</p>
+        ) :
+          <div >
+            <p className='noneList'>{selInt}의 요청서가 없습니다.</p>
           </div>
-        </div>
-      ) :
-        <div >
-          <p className='noneList'>{selInt}의 요청서가 없습니다.</p>
-        </div>
-      }
+        }
+      </div>
     </div >
   );
 };

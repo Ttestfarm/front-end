8ef2198c5c81d4b9f3fd21bff6407080965f7deb
Jisu <139774652+serendipity63@users.noreply.github.com>
2023-12-24 18:11:45 +0900

    quotepay 구현

--- front-test-farm/src/components/myPages/QuotePay.js
+++ front-test-farm/src/components/myPages/QuotePay.js
@@ -1,36 +1,25 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { tokenAtom, userInfoAtom } from './../../recoil/Atoms';
-import { importIamport, userCode } from '../../api/iamport';
-import * as API from '../../api/index';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { tokenAtom, userInfoAtom } from "./../../recoil/Atoms";
+import { importIamport, userCode } from "../../api/iamport";
+import * as API from "../../api/index";
+import { useParams, useNavigate } from "react-router-dom";
 
-const QuotePay = ({ quoteItem }) => {
+const QuotePay = ({}) => {
   const token = useRecoilValue(tokenAtom);
   const navigate = useNavigate();
 
   const quotationId = useParams().quotationId;
   const [userInfo, setUserInfo] = useRecoilState(userInfoAtom);
-  // const [paymentInfo, setPaymentInfo] = useState({
-  //   pg: 'html5_inicis',
-  //   pay_method: 'card',
-  //   name: quoteItem.productName,
-  //   merchant_uid: `mid_${new Date().getTime()}`, //orderId인것 같은데
-  //   amount: parseInt(quoteItem.quote.quotationPrice), //실제 결제되는 가격
-  //   buyer_name: userInfo.name, // 구매자 이름
-  //   buyer_tel: userInfo.phone, // 구매자 전화번호
-  //   buyer_email: userInfo.email, // 구매자 이메일
-  //   buyer_addr: userInfo.address, // 구매자 주소
-  // });
+  const [paymentInfo, setPaymentInfo] = useState(null);
 
   const [quoteData, setQuoteData] = useState(null);
   useEffect(() => {
-    console.log('dml??', quoteItem);
     const getQuote = async () => {
       try {
         const response = await API.get(`/user/request/${quotationId}`, token);
-        console.log('quotepay', response);
+        console.log("quotepay", response);
         setQuoteData({ ...response.data });
       } catch (error) {
         console.log(error);
@@ -39,17 +28,110 @@ const QuotePay = ({ quoteItem }) => {
 
     getQuote();
   }, []);
+  useEffect(() => {
+    if (quoteData) {
+      setPaymentInfo({
+        pg: "html5_inicis",
+        pay_method: "card",
+        name: quoteData.quote.quotation.quotationProduct, //상품명
+        merchant_uid: `mid_${new Date().getTime()}`,
+        amount: parseInt(quoteData.quote.quotation.quotationPrice),
+        buyer_name: quoteData.quote.request.name,
+        buyer_tel: quoteData.quote.request.tel,
+        buyer_addr: quoteData.quote.request.address1,
+      });
+    }
+  }, [quoteData]);
+
+  useEffect(() => {
+    const jquery = document.createElement("script");
+    jquery.src = "http://code.jquery.com/jquery-1.12.4.min.js";
+    const iamport = document.createElement("script");
+    iamport.src = "http://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
+    document.head.appendChild(jquery);
+    document.head.appendChild(iamport);
+    return () => {
+      document.head.removeChild(jquery);
+      document.head.removeChild(iamport);
+    };
+  }, []);
+  const requestPay = async () => {
+    try {
+      const { IMP } = window;
+      IMP.init(userCode);
+
+      const rsp = await new Promise((resolve, reject) => {
+        IMP.request_pay(paymentInfo, (response) => {
+          resolve(response);
+        });
+      });
+
+      console.log(rsp);
+
+      if (rsp.success) {
+        const res = await API.post2(`/payment/validation/${rsp.imp_uid}`);
+
+        if (
+          parseInt(quoteData.quote.quotation.quotationPrice) ===
+          res.data.response.amount
+        ) {
+          try {
+            const response = await API.post2(`/payment`, token, {
+              receiptId: rsp.imp_uid,
+              amount: rsp.paid_amount,
+              ordersId: rsp.merchant_uid,
+              buyerName: rsp.buyer_name,
+              buyerTel: rsp.buyer_tel,
+              buyerAddress: rsp.buyer_addr,
+              paymentMethod: rsp.pay_method,
+              pgTid: rsp.pg_tid,
+              pgType: rsp.pg_type,
+              status: rsp.status,
+              paidAt: rsp.paid_at,
+              productName: rsp.name,
+              quotationId: quoteData.quote.quotation.quotationId,
+              // productPrice: productPrice,
+              // count: quantity,
+              // productId: state.deliveryInfo.productId,
+              // farmerId: state.deliveryInfo.farmerId,
+              // paymentDelivery: state.deliveryInfo.paymentDelivery,
+            });
+
+            alert(response.data);
+          } catch (error) {
+            console.error("Error while processing payment:", error);
+            alert("Payment processing failed");
+          }
+        } else {
+          alert("Payment failed1");
+        }
+      } else {
+        alert("Payment failed2");
+      }
+    } catch (error) {
+      console.error("Error occurred during payment:", error);
+      alert("Error occurred during payment");
+    }
+  };
 
   return (
     <div>
       <h2>Quote Detail</h2>
-      {quoteData ? (
-        <div>
-          <p>Quotation ID: {quoteData.quote.id}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
+      {quoteData && (
+        <>
+          <p>Product: {quoteData.quote.quotation.quotationProduct}</p>
+          <p>Quantity: {quoteData.quote.quotation.quotationQuantity}</p>
+          <p>Price: {quoteData.quote.quotation.quotationPrice}</p>
+          주소
+          {quoteData.quote.request.address1}
+          {quoteData.quote.request.address2}
+          {quoteData.quote.request.address3}
+          {quoteData.quote.request.name}
+          {quoteData.quote.request.tel}
+          {/* 기타 데이터를 필요한대로 추가적으로 렌더링 */}
+        </>
       )}
+      <button onClick={requestPay}>결제하기</button>
     </div>
   );
 };
